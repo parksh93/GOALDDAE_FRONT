c9f9d6415c3ddf9b29fefe3696a5fecba551dbd8
SeoPBi <aktms252@naver.com>
2023-08-12 18:45:07 +0900

    신현섭 추가 : 메인페이지 이미지슬라이드

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,12 @@
 import LoginInfo from './LoginInfo';
+import ImageSlide from './auth/imageSlide/ImageSlide';
+
 const Main = () => {
     return(
         <div>
             <LoginInfo />
             <h1>메인</h1>
+            <ImageSlide />
         </div>
     )
 }
---
+++ src/auth/imageSlide/ImageSlide.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import Slider from "react-slick";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './ImageSlide.css';
+
+// 임시 이미지 불러오기 추후 백엔드에서 저장 예정
+import TestImg1 from "./image/testImg1.jpg";
+import TestImg2 from "./image/testImg2.jpg";
+import TestImg3 from "./image/testImg3.jpg";
+import TestImg4 from "./image/testImg4.jpg";
+import TestImg5 from "./image/testImg5.jpg";
+import TestImg6 from "./image/testImg6.jpg";
+
+const ImageSlide = () => {
+  const [settings] = useState({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />
+  });
+
+  return (
+    <div className="image-slide-container">
+      <Slider {...settings}>
+           <img src={TestImg1} alt="이미지" className="img" />
+           <img src={TestImg2} alt="이미지" className="img" />
+           <img src={TestImg3} alt="이미지" className="img" />
+           <img src={TestImg4} alt="이미지" className="img" />
+           <img src={TestImg5} alt="이미지" className="img" />
+           <img src={TestImg6} alt="이미지" className="img" />
+      </Slider>
+    </div>
+  );
+};
+
+const NextArrow = ({ onClick }) => {
+  return (
+    <button onClick={onClick} type='button' className="slick-next" />
+    
+  );
+};
+
+const PrevArrow = ({ onClick }) => {
+  return (
+    <button onClick={onClick} type='button' className="slick-prev" />
+  );
+};
+
+export default ImageSlide;
