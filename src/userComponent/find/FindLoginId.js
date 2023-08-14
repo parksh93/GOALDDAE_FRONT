9abfa444eec380ed5b19ff1915c6f372ef0b7ae1
parksh93 <sanghyun8527@gmail.com>
2023-08-14 17:51:33 +0900

    추가: REST를 통해 백엔드와 연결 및 인증 번호 입력 input 추가

--- src/userComponent/find/FindLoginId.js
+++ src/userComponent/find/FindLoginId.js
@@ -1,30 +1,147 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import { TextField } from "@mui/material";
 import { Button } from "@mui/material";
 import styled from "@emotion/styled";
 
-const FindLoginId = ({styles}) => {
+const FindLoginId = ({ styles, setSeverity, setOpen, setAlertTitle, setAlertContent }) => {
+  const [sendMailOk, setSendMailOk] = useState(false);
+  const [certified, setCertified] = useState(false);
+  const [nickname, setNickname] = useState("");
+  const [email, setEmail] = useState("");
+  const [certificationcode, setCertificationcode] = useState("");
+  const [certificationcodeCheck, setCertificationcodeCheck] = useState("");
+  const [errorMsg, setErrorMsg] = useState('');
+  const [okMsg,setOkMsg] = useState('');
+  const [loginId, setLoginId] = useState('');
+
+  const onChangeNickname = useCallback(
+    (e) => {
+      setNickname(e.target.value.trim());
+      setErrorMsg('');
+    },
+    [nickname]
+  );
+
+  const onChangeEmail = useCallback(
+    (e) => {
+      setEmail(e.target.value.trim());
+      setErrorMsg('');
+      setOkMsg('');
+    },
+    [email]
+    );
+    
+    const onChangCertificationCodeCheck = useCallback(
+      (e) => {
+        setCertificationcodeCheck(e.target.value.trim());
+        setErrorMsg('');
+        setOkMsg('');
+      },
+    [certificationcodeCheck]
+    );
+    
+    const onClickSendEmail = useCallback(() => {
+    if(nickname !== ""){
+      if(email !== ""){
+        fetch("/user/findLoginId", {
+          method: "POST",
+          headers: {
+            "Contnet-type": "application/json"
+          },
+          body: JSON.stringify({
+            nickname: nickname,
+            email: email
+          })
+        }).then(res => res.json())
+        .then(data => {
+          if(data !== null){
+            console.log(data);
+            setLoginId(data);
+
+            setOpen(true);
+            setSeverity("success");
+            setAlertTitle("인증번호 전송 완료");
+            setAlertContent("해당 이메일로 인증번호가 전송되었습니다.");
+            setTimeout(() => setOpen(false), 2000);
+
+            fetch(`/sendEmailFindLoginId/${email}`,{method: "get"})
+            .then(res => res.json())
+            .then(data => {
+              setCertificationcode(data);
+              setSendMailOk(true);
+            });
+          }else {
+            setOpen(true);
+            setSeverity("error");
+            setAlertTitle("아이디 찾기 실패");
+            setAlertContent("해당 닉네임과 이메일로 가입된 회원이 없습니다. 다시 확인해주세요.");
+            setTimeout(() => setOpen(false), 2000);
+          }
+        })
+      }else{
+        setErrorMsg("이메일을 입력해주세요.");
+      }
+    }else{
+      setErrorMsg("닉네임을 입력해주세요.");
+    }
+  });
+  
+  const onClickCheckCertifiCode = useCallback(() => {
+    if(certificationcodeCheck !== ""){
+      if(certificationcode === certificationcodeCheck){
+        setOkMsg("인증되었습니다.");
+        setCertified(true);
+      }
+    }else{
+      setErrorMsg("인증번호를 입력해주세요.");
+    }
+  });
   return (
-    <div className={styled.findMenuDiv}>
-      <TextField
-        label="닉네임"
-        variant="outlined"
-        type="text"
-        color="success"
-        className={styles.input}
-      />
-      <br />
-      <TextField
-        label="이메일"
-        variant="outlined"
-        type="text"
-        color="success"
-        className={styles.input}
-      />
-      <section className={styles.okBtn}>
-        <CheckBnt >
-          확인
-        </CheckBnt>
+    <div className={styles.findMenuDiv}>
+      <section className={styles.container}>
+        <TextField
+          label="닉네임"
+          variant="outlined"
+          type="text"
+          color="success"
+          className={styles.input}
+          value={nickname}
+          onChange={onChangeNickname}
+          style={{marginTop: '20px'}}
+        />
+        <br />
+        <TextField
+          label="이메일"
+          variant="outlined"
+          type="text"
+          color="success"
+          className={styles.input}
+          value={email}
+          onChange={onChangeEmail}
+          style={{marginTop: '20px'}}
+          />
+        <SendBtn color="success" onClick={onClickSendEmail}>{sendMailOk === true ? "재전송" : "인증번호" }</SendBtn>
+        <br />
+        {sendMailOk === true ? 
+          <div>
+            <TextField
+              label="인증번호"
+              variant="outlined"
+              type="text"
+              color="success"
+              className={styles.input}
+              value={certificationcodeCheck}
+              onChange={onChangCertificationCodeCheck}
+              style={{marginTop: '20px'}}
+            />
+            <SendBtn color="success" onClick={onClickCheckCertifiCode}>인증</SendBtn>
+          </div>
+         : "" }
+         <p className={styles.errorMsg}>{errorMsg}</p>
+         <p className={styles.okMsg}>{okMsg}</p>
+      </section>
+      <section className={styles.oKBtnSection}>
+        <CheckBnt className={styles.okBtn}>확인</CheckBnt>
       </section>
     </div>
   );
@@ -34,7 +151,7 @@ export default React.memo(FindLoginId);
 
 const CheckBnt = styled(Button)`
   top: 10px;
-  width: 45.9%;
+  width: 53%;
   display: block;
   height: 40px;
   border: 2px solid #108b0c;
@@ -43,3 +160,13 @@ const CheckBnt = styled(Button)`
   font-size: 16px;
 `;
 
+const SendBtn = styled(Button)`
+  right: 0;
+  width: 100px;
+  height: 55px;
+  border: 1px solid green;
+  color: green;
+  font-size: 16px;
+  margin-left: 10px;
+  margin-top: 20px;
+`;
--- src/userComponent/find/FindMain.js
+++ src/userComponent/find/FindMain.js
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import Button from "@mui/material/Button";
 import { useParams, Link } from "react-router-dom";
 import styles from "./Find.module.css";
 import FindLoginId from "./FindLoginId";
-
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
+import Collapse from "@mui/material/Collapse";
 
 const FindMain = () => {
   let { findMenuNum } = useParams();
@@ -13,8 +15,75 @@ const FindMain = () => {
   }
 
   const [findMenu, setFindMenu] = useState(findMenuNum);
+  const [idStyle, setIdStyle] = useState({
+    background: "",
+    color: "",
+  });
+  const [pwStyle, setPwStyle] = useState({
+    background: "",
+    color: "",
+  });
+  const [open, setOpen] = useState(false);
+  const [severity, setSeverity] = useState('');
+  const [alertTitle, setAlertTitle] = useState('');
+  const [alertContent, setAlertContent] = useState('');
+
+  useEffect(() => {
+    if (findMenu === "1") {
+      setIdStyle({
+        background: "green",
+        color: "white",
+      });
+      setPwStyle({
+        background: "white",
+        color: "green",
+      });
+    }else{
+      setIdStyle({
+        background: "white",
+        color: "green",
+      });
+      setPwStyle({
+        background: "green",
+        color: "white",
+      });
+    }
+  }, []);
+
+  const onClickId = useCallback(() => {
+    setFindMenu("1");
+    setIdStyle({
+      background: "green",
+      color: "white",
+    });
+    setPwStyle({
+      background: "white",
+      color: "green",
+    });
+  });
+
+  const onClickPw = useCallback(() => {
+    setFindMenu("2");
+    setIdStyle({
+      background: "white",
+      color: "green",
+    });
+    setPwStyle({
+      background: "green",
+      color: "white",
+    });
+  });
+
   return (
     <div>
+      <Collapse in={open}>
+        <Alert severity={severity}>
+          <AlertTitle>
+            <b>{alertTitle}</b>
+          </AlertTitle>
+          {alertContent}          
+        </Alert>
+      </Collapse>
       <section className={styles.logoSection}>
         <Link to="/">
           <img src="../img/goalddaeLogo.png" className={styles.logo} />
@@ -23,23 +92,35 @@ const FindMain = () => {
       <section className={styles.findBtnSection}>
         <Button
           size="large"
-          onClick={() => setFindMenu("1")}
+          onClick={onClickId}
           color="success"
           className={styles.findIdBtn}
+          style={idStyle}
         >
           아이디 찾기
         </Button>
         <Button
           size="large"
-          onClick={() => setFindMenu("2")}
+          onClick={onClickPw}
           color="success"
           className={styles.findPwBtn}
+          style={pwStyle}
         >
           비밀번호 찾기
         </Button>
       </section>
       <section className={styles.mainSession}>
-        {findMenu === "1" ? <FindLoginId styles={styles}/> : "비밀번호 찾기화면"}
+        {findMenu === "1" ? (
+          <FindLoginId 
+            styles={styles} 
+            setOpen={setOpen}
+            setSeverity = {setSeverity}
+            setAlertTitle= {setAlertTitle}
+            setAlertContent = {setAlertContent}
+          />
+        ) : (
+          "비밀번호 찾기화면"
+        )}
       </section>
     </div>
   );
