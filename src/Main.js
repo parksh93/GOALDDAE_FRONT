a32f392bfb7377ffed3010e26ea93ce4f7456be9
SeoPBi <aktms252@naver.com>
2023-08-16 21:13:33 +0900

    신현섭: 수정 타임라인 동적테이블 연결 테스트 중

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { UserProvider } from "./userComponent/userContext/UserContext";
 import ImageSlide from './auth/imageSlide/ImageSlide';
+import TimeLine from './auth/timeLine/TimeLine';
 
 const Main = () => {
     return (
@@ -8,6 +9,7 @@ const Main = () => {
             <div>
                 <h1>메인</h1>
                 <ImageSlide />
+                <TimeLine />
             </div>
         </UserProvider>
     );
