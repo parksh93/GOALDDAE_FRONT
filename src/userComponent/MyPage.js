74971f46754a26dfd12f2d3dd2a10481deffe4ae
deepoceanvibe <d22pocean@gmail.com>
2023-08-16 14:46:16 +0900

    트러블 슈팅

--- src/userComponent/MyPage.js
+++ src/userComponent/MyPage.js
@@ -1,297 +1,84 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {UserProvider} from "./userComponent/userContext/UserContext";
+import { useUser, UserProvider } from './userContext/UserContext';
 
 function Mypage() {
+  const { userInfo, setUserInfo } = useUser();
+  const [isEditing, setIsEditing] = useState(false);
 
-  // 조회
-  const [profilePicture, setProfilePicture] = useState(null);
-  const [nickName, setnickName] = useState('');
-  const [email, setEmail] = useState('');
-  const [gender, setGender] = useState('');
-  const [level, setLevel] = useState('');
-  const [birth, setBirth] = useState('');
-  const [noshowCnt, setNoshowCnt] = useState('');
-  const [match, setMatch] = useState('');
-  const [posts, setPosts] = useState('');
-  const [preferredCity, setPreferredCity] = useState('');
-  const [preferredArea, setPreferredArea] = useState('');
-  const [activityClass, setActivityClass] = useState('');
-  const [friendsList, setFriendsList] = useState('');
-
-  // 수정
-  const handlePictureUpload = (event) => {
-    const file = event.target.files[0];
-    setProfilePicture(URL.createObjectURL(file));
-  };
-  const handleNickNameChange = (event) => {
-    setnickName(event.target.value);
-  };
-  const handlEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
-  const handleGenderChange = (event) => {
-    setGender(event.target.value);
-  };
-  const handleLevelChange = (event) => {
-    setLevel(event.target.value);
-  };
-  const handleBirthChange = (event) => {
-    setBirth(event.target.value);
-  };
-  const handleNoshowCntChange = (event) => {
-    setNoshowCnt(event.target.value);
-  };
-  const handleMatchChange = (event) => {
-    setMatch(event.target.value);
-  };
-  const handlePostsChange = (event) => {
-    setPosts(event.target.value);
-  };
-  const handlePreferredCityChange = (event) => {
-    setPreferredCity(event.target.value);
-  };
-  const handlePreferredAreaChange = (event) => {
-    setPreferredArea(event.target.value);
-  };
-  const handleActivityClassChange = (event) => {
-    setActivityClass(event.target.value);
-  };
-  const handleFriendsListChange = (event) => {
-    setFriendsList(event.target.value);
+  const handleEditClick = () => {
+    setIsEditing(true);
   };
 
-
-
-const [isEditing, setIsEditing] = useState(false); // 현재 수정 모드인지 여부
-const [editingField, setEditingField] = useState(''); // 수정 중인 항목을 저장할 상태
-
-
-  // 수정 모드 토글 함수
-  const toggleEditMode = (field) => {
-    if (isEditing && editingField === field) {
-      setIsEditing(false);
-      setEditingField('');
-    } else {
-      setIsEditing(true);
-      setEditingField(field);
-    }
+  const handleCancelClick = () => {
+    setIsEditing(false);
   };
 
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.post('/user/getUserInfo');
+        if (response.status === 200) {
+          setUserInfo(response.data); // 유저 정보 업데이트
+        } else {
+          console.error('Error fetching user data:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
 
-// 저장 함수 (이곳에서 서버로 정보 업데이트 요청을 보낼 수 있음)
-const saveChanges = () => {
-  setIsEditing(false);
-// 서버로 정보 업데이트 요청 등 추가 작업 수행
-};
-
-
+    fetchUserData();
+  }, [setUserInfo]); // setUserInfo를 의존성 배열에 추가
 
   return (
-    <div className="my-page">
-      <h2>마이페이지</h2>
-      <div className="user-info">
-        {/* 프로필사진 */}
-        {profilePicture && <img src={profilePicture} alt="Profile" />}
-        {isEditing  ? (
-          <>
-          프로필사진 :
-            <input type="file" accept="image/*" onChange={handlePictureUpload} />
-            <button onClick={toggleEditMode}>취소</button>
-            <button onClick={saveChanges}>저장</button>
-          </>
-        ) : (
-          <label>
-            프로필사진 :
-            <button onClick={toggleEditMode}>수정</button>
-          </label>
-        )}
+    <UserProvider>
+      <div className="my-page">
+        <h2>마이페이지</h2>
+        <div className="user-info">
+          {userInfo ? (
+            <React.Fragment>
+              {isEditing ? (
+                <React.Fragment>
+                  프로필사진 : <input type="file" />
+                  닉네임 : <input type="text" defaultValue={userInfo.nickName} />
+                  {/* ... */}
+                </React.Fragment>
+              ) : (
+                <React.Fragment>
+                  프로필사진 : <img src={userInfo.profilePicture} alt="프로필 사진" />
+                  닉네임 : {userInfo.nickName}
+                  이메일 : {userInfo.email}
+                  성별 : {userInfo.gender}
+                  레벨 : {userInfo.level}
+                  생년월일 : {userInfo.birth}
+                  노쇼횟수 : {userInfo.noshowCnt}
+                  매치리스트 : {userInfo.match}
+                  게시글리스트 : {userInfo.posts}
+                  선호도시 : {userInfo.preferredCity}
+                  선호지역 : {userInfo.preferredArea}
+                  활동반경 : {userInfo.activityClass}
+                  친구리스트 : {userInfo.friendsList}
+                </React.Fragment>
+              )}
+            </React.Fragment>
+          ) : (
+            <p>유저 정보를 불러오는 중...</p>
+          )}
+        </div>
 
-        {/* 이름 */}
-        {isEditing && editingField === 'nickName' ? (
-          <>
-          이름 :
-            <input type="text" value={nickName} onChange={handleNickNameChange} />
-            <button onClick={toggleEditMode}>취소</button>
-            <button onClick={saveChanges}>저장</button>
-          </>
-        ) : (
-          <label>
-            이름 :
-            {nickName}
-            <button onClick={toggleEditMode}>수정</button>
-          </label>
-        )}
-
-        {/* 이메일 */}
-        {isEditing ? (
-          <>
-          이메일 :
-            <input type="email" value={email} onChange={handlEmailChange} />
-            <button onClick={toggleEditMode}>취소</button>
-            <button onClick={saveChanges}>저장</button>
-          </>
-        ) : (
-          <label>
-            이메일 :
-            {email}
-            <button onClick={toggleEditMode}>수정</button>
-          </label>
-        )}
-
-        {/* 성별 */}
-        {isEditing ? (
-          <>
-          성별 :
-            <input type="text" value={gender} onChange={handleGenderChange} />
-            <button onClick={toggleEditMode}>취소</button>
-            <button onClick={saveChanges}>저장</button>
-          </>
-        ) : (
-          <label>
-            성별 :
-            {gender}
-            <button onClick={toggleEditMode}>수정</button>
-          </label>
-        )}
-
-       {/* 레벨 */}
-       {isEditing ? (
-          <> 레벨 : 
-            <input type="text" value={level} onChange={handleLevelChange} />
-            <button onClick={() => toggleEditMode('')}>취소</button>
-            <button onClick={saveChanges}>저장</button>
-          </>
-        ) : (
-          <label>
-            레벨 :
-            {level}
-            <button onClick={() => toggleEditMode('level')}>수정</button>
-          </label>
-        )}
-
-        {/* 생년월일 */}
-        {isEditing  ? (
-          <> 생년월일 :
-            <input type="text" value={birth} onChange={handleBirthChange} />
-            <button onClick={() => toggleEditMode('')}>취소</button>
-            <button onClick={saveChanges}>저장</button>
-          </>
-        ) : (
-          <label>
-            생년월일 :
-            {birth}
-            <button onClick={() => toggleEditMode('birth')}>수정</button>
-          </label>
-        )}
-
-        {/* 노쇼횟수 */}
-        {isEditing ? (
-          <> 노쇼횟수
-            <input type="text" value={noshowCnt} onChange={handleNoshowCntChange} />
-            <button onClick={() => toggleEditMode('')}>취소</button>
-            <button onClick={saveChanges}>저장</button>
-          </>
-        ) : (
-          <label>
-            노쇼횟수 :
-            {noshowCnt}
-            <button onClick={() => toggleEditMode('noshowCnt')}>수정</button>
-          </label>
-        )}
-
-        {/* 매치리스트 */}
-        {isEditing  ? (
-          <> 매치리스트 :
-            <input type="text" value={match} onChange={handleMatchChange} />
-            <button onClick={() => toggleEditMode('')}>취소</button>
-            <button onClick={saveChanges}>저장</button>
-          </>
-        ) : (
-          <label>
-            매치리스트 :
-            {match}
-            <button onClick={() => toggleEditMode('match')}>수정</button>
-          </label>
-        )}
-
-        {/* 게시글리스트 */}
-        {isEditing  ? (
-          <> 게시글리스트 : 
-            <input type="text" value={posts} onChange={handlePostsChange} />
-            <button onClick={() => toggleEditMode('')}>취소</button>
-            <button onClick={saveChanges}>저장</button>
-          </>
-        ) : (
-          <label>
-            게시글리스트 :
-            {posts}
-            <button onClick={() => toggleEditMode('posts')}>수정</button>
-          </label>
-        )}
-
-        {/* 선호도시 */}
-        {isEditing  ? (
-          <> 선호도시 :
-            <input type="text" value={preferredCity} onChange={handlePreferredCityChange} />
-            <button onClick={() => toggleEditMode('')}>취소</button>
-            <button onClick={saveChanges}>저장</button>
-          </>
-        ) : (
-          <label>
-            선호도시 :
-            {preferredCity}
-            <button onClick={() => toggleEditMode('preferredCity')}>수정</button>
-          </label>
-        )}
-
-          {/* 선호지역 */}
-          {isEditing  ? (
-          <> 선호지역 :
-            <input type="text" value={preferredArea} onChange={handlePreferredAreaChange} />
-            <button onClick={() => toggleEditMode('')}>취소</button>
-            <button onClick={saveChanges}>저장</button>
-          </>
-        ) : (
-          <label>
-            선호지역 :
-            {preferredArea}
-            <button onClick={() => toggleEditMode('preferredArea')}>수정</button>
-          </label>
-        )}
-
-        {/* 활동반경 */}
-        {isEditing  ? (
-          <> 활동반경 :
-            <input type="text" value={activityClass} onChange={handleActivityClassChange} />
-            <button onClick={() => toggleEditMode('')}>취소</button>
-            <button onClick={saveChanges}>저장</button>
-          </>
-        ) : (
-          <label>
-            활동반경 :
-            {activityClass}
-            <button onClick={() => toggleEditMode('activityClass')}>수정</button>
-          </label>
-        )}
-
-        {/* 친구리스트 */}
-        {isEditing ? (
-          <> 친구리스트 :
-            <input type="text" value={friendsList} onChange={handleFriendsListChange} />
-            <button onClick={() => toggleEditMode('')}>취소</button>
-            <button onClick={saveChanges}>저장</button>
-          </> 
-        ) : (
-          <label>
-            친구리스트 :
-            {friendsList}
-            <button onClick={() => toggleEditMode('friendsList')}>수정</button>
-          </label>
-        )}
-
+        <div className="buttons">
+          {isEditing ? (
+            <React.Fragment>
+              <button onClick={handleCancelClick}>취소</button>
+              <button>저장</button>
+            </React.Fragment>
+          ) : (
+            <button onClick={handleEditClick}>수정</button>
+          )}
+        </div>
       </div>
-    </div>
+    </UserProvider>
   );
 }
 
