2b2756640943b57a2948a07e815bde4f2a37edd4
deepoceanvibe <d22pocean@gmail.com>
2023-08-21 11:59:11 +0900

    추가 : 마이페이지 및 유저수정

--- src/userComponent/MyPage.js
+++ src/userComponent/MyPage.js
@@ -1,64 +1,61 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useUser, UserProvider } from './userContext/UserContext';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+
+
 
 function Mypage() {
   const { userInfo, setUserInfo } = useUser();
-  const [isEditing, setIsEditing] = useState(false);
-  const [editedUserInfo, setEditedUserInfo] = useState({});
+  const [ isEditing, setIsEditing ] = useState(false);
+  const [rangeValue, setRangeValue] = useState(10);
 
-
-  const handleEditClick = () => {
-    // 수정 상태로 바꼈는지 로그
-    console.log("Edit button clicked");
-
-    setIsEditing(true);
+  const handleChange = (event) => {
+    const value = parseInt(event.target.value, 10);
+    setRangeValue(value);
+    handleInputChange('activityClass', value);
   };
 
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
   const handleCancelClick = () => {
     setIsEditing(false);
   };
+  
 
 
-  // 백엔드로 수정 요청
+  // 백엔드로 전달
   const handleSaveClick = async () => {
 
-    // 로그 백엔드로 수정요청 잘들어갔는지 확인
-    console.log("Save button clicked");
+      const response = await axios.put('/user/update', userInfo);
 
-    try {
-      const response = await axios.post('/user/update', editedUserInfo);
-
-      // 로그 백엔드 응답 확인
-      console.log('Backend response:', response.data);
-
-
+      // 컨트롤러로 요청 넣는 부분
+      console.log('업데이트 요청 응답 - status:', response.status, 'data:', response.data);
+      
       if (response.status === 200) {
-        setUserInfo({ ...userInfo, ...editedUserInfo }); // 사용자 정보 업데이트
+        setUserInfo(userInfo);
         setIsEditing(false);
+        console.log('업데이트된 정보 : ' + JSON.stringify(userInfo));
       } else {
-        console.error('Error updating user data:', response.statusText);
+        console.error('수정 에러 : ', response.statusText);
       }
-    } catch (error) {
-      console.error('Error updating user data:', error);
-    }
   };
 
+  // 수정한 값 전달
   const handleInputChange = (key, value) => {
-    // 인풋값 변경 로그
-    console.log(`Changing ${key} to ${value}`);
-
-    setEditedUserInfo(prev => ({ ...prev, [key]: value }));
+    setUserInfo(prev => ({ ...prev, [key]: value }));
   };
-
-
-
+  
+  // 유저정보 불러오기
   useEffect(() => {
     const fetchUserData = async () => {
       try {
         const response = await axios.post('/user/getUserInfo');
         if (response.status === 200) {
-          setUserInfo(response.data); // 유저 정보 업데이트
+          setUserInfo(response.data);
         } else {
           console.error('Error fetching user data:', response.statusText);
         }
@@ -68,7 +65,9 @@ function Mypage() {
     };
 
     fetchUserData();
-  }, [setUserInfo]); // setUserInfo를 의존성 배열에 추가
+  }, [setUserInfo]);
+
+
 
   return (
     <UserProvider>
@@ -79,29 +78,64 @@ function Mypage() {
             <React.Fragment>
               {isEditing ? (
                 <React.Fragment>
-                  닉네임 : <input type="text" defaultValue={editedUserInfo.nickName || userInfo.nickName} onChange={(e) => handleInputChange('nickName', e.target.value)} />
-                  이메일 : <input type="text" defaultValue={editedUserInfo.email || userInfo.email} onChange={(e) => handleInputChange('email', e.target.value)} />
-                  전화번호 : <input type="text" defaultValue={editedUserInfo.phoneNumber || userInfo.phoneNumber} onChange={(e) => handleInputChange('phoneNumber', e.target.value)} />
-                  생년월일 : <input type="text" defaultValue={editedUserInfo.birth || userInfo.birth} onChange={(e) => handleInputChange('birth', e.target.value)} />
-                  선호도시 : <input type="text" defaultValue={editedUserInfo.preferredCity || userInfo.preferredCity} onChange={(e) => handleInputChange('preferredCity', e.target.value)} />
-                  선호지역 : <input type="text" defaultValue={editedUserInfo.preferredArea || userInfo.preferredArea} onChange={(e) => handleInputChange('preferredArea', e.target.value)} />
-                  활동반경 : <input type="text" defaultValue={editedUserInfo.activityClass || userInfo.activityClass} onChange={(e) => handleInputChange('activityClass', e.target.value)} />
+                  닉네임 : <input type="text" defaultValue={userInfo.nickname} onChange={(e) => handleInputChange('nickname', e.target.value)} />
+                  이메일 : <input type="text" defaultValue={userInfo.email} onChange={(e) => handleInputChange('email', e.target.value)} />
+                  전화번호 : <input type="text" defaultValue={userInfo.phoneNumber} onChange={(e) => handleInputChange('phoneNumber', e.target.value)} />
+                  생년월일 : <input type="text" defaultValue={userInfo.birth} onChange={(e) => handleInputChange('birth', e.target.value)} />
+                  선호도시 : <input type="text" defaultValue={userInfo.preferredCity} onChange={(e) => handleInputChange('preferredCity', e.target.value)} />
+                  선호지역 : <Form.Select defaultValue={userInfo.preferredArea} onChange={(e) => 
+                    handleInputChange('preferredArea', e.target.value)}>
+                      <option value="">선호지역을 선택하세요</option>
+                      <option value="서울">서울</option>
+                      <option value="경기">경기</option>
+                      <option value="인천">인천</option>
+                      <option value="강원">강원</option>
+                      <option value="대전">대전</option>
+                      <option value="충남">충남</option>
+                      <option value="충북">충북</option>
+                      <option value="대구">대구</option>
+                      <option value="경북">경북</option>
+                      <option value="부산">부산</option>
+                      <option value="울산">울산</option>
+                      <option value="경남">경남</option>
+                      <option value="광주">광주</option>
+                      <option value="전남">전남</option>
+                      <option value="전북">전북</option>
+                      <option value="제주">제주</option>
+                            </Form.Select>
+                            <h5>활동반경: {rangeValue}</h5>
+                              <Form>
+                                <Form.Group controlId="formRange">
+                                  <Form.Label>Range</Form.Label>
+                                  <Form.Range
+                                    min="10"
+                                    max="20"
+                                    step="5"
+                                    value={rangeValue}
+                                    onChange={handleChange}
+                                  />
+                                </Form.Group>
+                              </Form>
                 </React.Fragment>
               ) : (
+
+                // 사용자 UI
                 <React.Fragment>
-                  <p> 프로필사진 : <img src={userInfo.profilePicture} alt="프로필 사진" /></p>
-                  <p>닉네임 : {userInfo.nickName} </p>
+                <Col xs={6} md={4}>
+                  <Image src="holder.js/171x180" roundedCircle />
+                </Col>
+                  <p><b>{userInfo.nickname}</b></p>
+                  <p>{userInfo.gender} </p>
+                  <p>{userInfo.level} </p>
                   <p>이메일 : {userInfo.email} </p>
-                  <p>성별 : {userInfo.gender} </p>
-                  <p>레벨 : {userInfo.level} </p>
                   <p>생년월일 : {userInfo.birth} </p>
                   <p>전화번호 : {userInfo.phoneNumber} </p>
-                  <p>노쇼횟수 : {userInfo.noshowCnt} </p>
-                  <p>매치리스트 : {userInfo.match} </p>
-                  <p>게시글리스트 : {userInfo.posts} </p>
                   <p>선호도시 : {userInfo.preferredCity} </p>
                   <p>선호지역 : {userInfo.preferredArea} </p>
                   <p>활동반경 : {userInfo.activityClass} </p>
+                  <p>노쇼횟수 : {userInfo.noshowCnt} </p>
+                  <p>매치리스트 : {userInfo.match} </p>
+                  <p>게시글리스트 : {userInfo.posts} </p>
                   <p>친구리스트 : {userInfo.friendsList} </p>
                 </React.Fragment>
               )}
