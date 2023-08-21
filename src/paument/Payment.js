7f733ae902e9ac4d2a56c33e53a5aa9e8dfb275e
parksh93 <sanghyun8527@gmail.com>
2023-08-21 21:26:05 +0900

    추가: 가격 검증 REST 요청 추가

--- src/paument/Payment.js
+++ src/paument/Payment.js
@@ -1,65 +1,98 @@
-import React from 'react'
-import {useEffect, useState, useContext} from 'react'
-import styles from './Payment.module.css'
-import {useNavigate} from 'react-router-dom';
-import {useUser} from '../userComponent/userContext/UserContext';
+import React from "react";
+import { useEffect, useState, useContext } from "react";
+import styles from "./Payment.module.css";
+import { useNavigate } from "react-router-dom";
+import { useUser } from "../userComponent/userContext/UserContext";
 
-const Payment = ({fieldName, reservationFree}) => {
-    const navigete = useNavigate();
-    const {userInfo} = useUser();
-    const  [franchiseKey, setFranchiseKey] = useState('');
-    const [pg, setPg] = useState('');
+const Payment = ({ fieldId, fieldName, reservationFree }) => {
+  const navigete = useNavigate();
+  const { userInfo } = useUser();
+  const [franchiseKey, setFranchiseKey] = useState("");
 
-    useEffect(() => {
-        fetch("/payment/getPaymentProperties")
-        .then(res => res.json())
-        .then(data => {
-            setFranchiseKey(data.franchiseKey);
-            setPg(data.pg);
-        })
-        
-    },[]);
+  useEffect(() => {
+    fetch("/payment/getPaymentProperties")
+      .then((res) => res.json())
+      .then((data) => {
+        setFranchiseKey(data.franchiseKey);
+      });
+  }, []);
 
-    let IMP = window.IMP; 
-    IMP.init(franchiseKey); 
+  let IMP = window.IMP;
+  IMP.init(franchiseKey);
 
-    var today = new Date();   
-    var hours = today.getHours(); 
-    var minutes = today.getMinutes();
-    var seconds = today.getSeconds();
-    var milliseconds = today.getMilliseconds();
-    var makeMerchantUid = hours +  minutes + seconds + milliseconds;
+  var today = new Date();
+  var hours = today.getHours();
+  var minutes = today.getMinutes();
+  var seconds = today.getSeconds();
+  var milliseconds = today.getMilliseconds();
+  var makeMerchantUid = hours + minutes + seconds + milliseconds;
 
-
-    function requestPayKakao() {
-        if(userInfo !== null){
-            IMP.request_pay({
-                pg : pg,
-                pay_method : 'card',
-                merchant_uid: "IMP"+makeMerchantUid,
-                name : fieldName,
-            amount : reservationFree,
-            buyer_email : userInfo.email,
-            buyer_name : userInfo.name,
-            buyer_tel : userInfo.phoneNumber,   //필수 파라미터
-            }, rsp => {
-                console.log(rsp)
-                if(rsp.success){
-                    alert("결제 완료");
-                }else{
-                    alert("결제 실패");
+  function requestPayKakao() {
+    if (userInfo !== null) {
+      IMP.request_pay(
+        {
+          pg: "kakaopay.TC0ONETIME",
+          pay_method: "card",
+          merchant_uid: "GOALDDAEIMP" + makeMerchantUid,
+          name: fieldName,
+          amount: reservationFree,
+          buyer_email: userInfo.email,
+          buyer_name: userInfo.name,
+          buyer_tel: userInfo.phoneNumber,
+        },
+        (rsp) => {
+          if (rsp.success) {
+            fetch("/payment/verifyIamPort", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                impUid: rsp.imp_uid,
+                amount: rsp.paid_amount,
+                fieldId: fieldId,
+              }),
+            })
+              .then((res) => {
+                if (res.status !== 200) {
+                  throw new Error();
                 }
-            });
-        }else{
-            alert("로그인이 후 이용 가능합니다.");
-            navigete("/login");
+
+                alert("결제 성공");
+              })
+              .catch(() => {
+                fetch("/payment/cancelPayment", {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify({
+                    impUid: rsp.imp_uid,
+                    reason: "결제 금액 위/변조",
+                    checkSum: rsp.paid_amount,
+                    refundHolder: rsp.buyer_name,
+                  }),
+                });
+                alert("결제 실패: 결제 금액 다름");
+              });
+          }
         }
-    }  
-    return (
-        <div className={styles.kakaopayBtnDiv}>
-            <button onClick={requestPayKakao} className={styles.kakopayBtn}><img src='../img/kakaopay.Webp'/></button>
-        </div>
-    );
-}
+      );
+    } else {
+      alert("로그인이 후 이용 가능합니다.");
+      navigete("/login");
+    }
+  }
+  return (
+    <div>
+
+      <div className={styles.kakaopayBtnDiv}>
+        <button onClick={requestPayKakao} className={styles.kakopayBtn}>
+          <img src="../img/kakaopay.Webp" />
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default React.memo(Payment);
