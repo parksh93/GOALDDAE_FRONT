7cc12ad0804593069c973de3410103e6b33aedb0
SeoPBi <aktms252@naver.com>
2023-08-28 14:43:16 +0900

    신현섭:완료 관리자페이지- 구장CRUD UI구성 완료

--- src/auth/page/admin/tableSoccerField/SoccerFieldDelete.js
+++ src/auth/page/admin/tableSoccerField/SoccerFieldDelete.js
@@ -1,60 +1,80 @@
 import React, { useEffect, useState } from 'react';
+import { Box, Button, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
 import axios from 'axios';
 
 const SoccerFieldDelete = () => {
-    const [soccerFields, setSoccerFields] = useState([]);
-    const [selectedFieldId, setSelectedFieldId] = useState('');
-  
-    useEffect(() => {
-      fetchSoccerFields();
-    }, []);
-  
-    const fetchSoccerFields = async () => {
+  const [soccerField, setSoccerField] = useState([]);
+  const [selectFieldId, setselectFieldId] = useState('');
+  const [openDialog, setOpenDialog] = useState(false);
+
+  useEffect(() => {
+    fetchSoccerField();
+  }, []);
+
+  const fetchSoccerField = async () => {
+    try {
+      const response = await axios.get('/soccerField/search');
+      setSoccerField(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleOpenDialog = () => {
+    setOpenDialog(true);
+  };
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+  };
+
+  const handleDelete = async () => {
+    if (!selectFieldId) return;
+
+    const confirmation = window.confirm('정말로 해당 구장을 제거하시겠습니까?');
+
+    if (confirmation) {
       try {
-        const response = await axios.get('/soccerField/search');
-        setSoccerFields(response.data);
+        await axios.post('/soccerField/delete', { id: selectFieldId });
+        alert('구장이 성공적으로 제거되었습니다.');
+        setselectFieldId('');
+        fetchSoccerField();
       } catch (error) {
         console.error(error);
+        alert('구장 제거 중 오류가 발생했습니다.');
       }
-    };
-  
-    const handleDelete = async () => {
-      if (!selectedFieldId) return;
-      
-      const confirmation = window.confirm('정말로 해당 구장을 제거하시겠습니까?');
-      
-      if (confirmation) {
-        try {
-          await axios.post('/soccerField/delete', { id: selectedFieldId });
-          alert('구장이 성공적으로 제거되었습니다.');
-          setSelectedFieldId(''); 
-          fetchSoccerFields(); 
-        } catch (error) {
-          console.error(error);
-          alert('구장 제거 중 오류가 발생했습니다.');
-        }
-      }
-    };
-  
-    return (
-      <div>
-        <h1>구장 삭제</h1>
-  
-         <select value={selectedFieldId} onChange={(e) => setSelectedFieldId(e.target.value)}>
-           <option value="">-- 구장 선택 --</option>
-           {soccerFields.map(field => (
-             <option key={field.id} value={field.id}>{field.fieldName}</option>
-           ))}
-         </select>
-  
-         {selectedFieldId && (
-           <>
-             <button onClick={handleDelete}>선택한 구장 제거</button>
-           </>
-         )}
-         
-       </div>
-     );
+    }
   };
+
+   return (
+     <div >
+      <h1 style={{ textAlign: 'center' }}>구장 삭제</h1>
+      <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, justifyContent: 'center', alignItems: 'center' }}>
+       <select value={selectFieldId} onChange={(e) => setselectFieldId(e.target.value)}>
+         <option value="">-- 구장 선택 --</option>
+         {soccerField.map(field => (
+           <option key={field.id} value={field.id}>{field.fieldName}</option>
+         ))}
+       </select>
+
+       {selectFieldId && (
+         <>
+           <Button variant="contained" color="success" onClick={handleOpenDialog}>
+            선택한 구장 제거
+          </Button>
+           <Dialog open={openDialog} onClose={handleCloseDialog}>
+             <DialogTitle>구장 제거</DialogTitle>
+             <DialogContent>정말로 해당 구장을 제거하시겠습니까?</DialogContent>
+             <DialogActions>
+               <Button onClick={handleCloseDialog}>취소</Button>
+               <Button onClick={handleDelete} autoFocus>제거</Button>
+             </DialogActions>
+           </Dialog>
+         </>
+       )}
+       </Box>
+     </div>
+   );
+};
 
 export default SoccerFieldDelete;
--- src/auth/page/admin/tableSoccerField/SoccerFieldTable.js
+++ src/auth/page/admin/tableSoccerField/SoccerFieldTable.js
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
-import { FormControl, FormControlLabel, Radio, RadioGroup, FormLabel } from '@mui/material';
+import { Alert ,Snackbar ,Paper ,Button, FormControl, FormControlLabel, Radio, RadioGroup, FormLabel } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useNavigate } from 'react-router-dom';
 import './SoccerFieldTable.css';
 
 const SoccerFieldTable = () => {
@@ -19,6 +20,8 @@ const SoccerFieldTable = () => {
   const [inOutWhether, setInOutWhether] = useState(undefined);
   const [grassWhether, setGrassWhether] = useState(undefined);
   const [region, setRegion] = useState('');
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const [fieldImg1Preview, setFieldImg1Preview] = useState(null);
   const [fieldImg2Preview, setFieldImg2Preview] = useState(null);
@@ -58,20 +61,27 @@ const SoccerFieldTable = () => {
         grassWhether: grassWhether, 
         region: region,
       };
-
+  
       const response = await axios.post('/soccerField/save', soccerField);
   
-      if (response.status === 200) { 
+      if (response.status === 200) {
         console.log(`생성 완료: ${fieldName}`);
-      } else { 
+        setOpenSnackbar(true);
+        setTimeout(() => {
+          setOpenSnackbar(false);
+        }, 3000);
+      } else {
         console.error(`생성 실패, 상태 코드: ${response.status}`);
-        alert('구장 추가에 실패했습니다.');
+        setErrorMessage(`구장 생성에 실패했습니다. ${fieldName} 생성에 실패했습니다.`);
+        setOpenSnackbar(true);
       }
-    } catch (error) { 
+    } catch (error) {
       console.error('구장 추가 중 에러 발생:', error);
-      alert('구장 추가에 실패했습니다. 다시 시도해주세요.');
+      setErrorMessage('구장 추가에 실패했습니다. 다시 시도해주세요.');
+      setOpenSnackbar(true);
     }
   };
+  
 
   const theme = createTheme({
     palette: {
@@ -81,157 +91,173 @@ const SoccerFieldTable = () => {
     },
   });
 
+  const navigate = useNavigate();
+
+
   return (
     <ThemeProvider theme={theme}>
       <h1 style={{ textAlign: 'center' }}>구장 등록</h1>
-        <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent:'center'}}>
-            <div className="required-box" style={{ display: 'flex', flexDirection: 'column'}}>
-              <div className="required-label">필수 입력</div>
-              <Box mb={2}>
-                <TextField
-                  value={fieldName}
-                  onChange={(e) =>setFieldName(e.target.value)}
-                  label="구장 이름"
-                  variant="outlined"
-                />
-              </Box>
-              <Box mb={2}>
-                <TextField
-                  value={region}
-                  onChange={(e) => setRegion(e.target.value)}
-                  label="지역"
-                  variant="outlined"
-                />
-              </Box>
-              <Box mb={2}>
-                <TextField
-                  value={fieldSize}
-                  onChange={(e) => setFieldSize(e.target.value)}
-                  label="구장 크기"
-                  variant="outlined"
-                />
-              </Box>
-              <Box mb={2}>
-                <TextField
-                  value={reservationFee}
-                  onChange={(e) => setReservationFee(e.target.value)}
-                  label="대관비"
-                  variant="outlined"
-                />
-              </Box>
-          </div>
-
-          <div className="required-box" style={{ display: 'flex', flexDirection: 'column'}}>
-              <div className="required-label">필수 선택</div>
-            <Box mb={2}>
-              <FormControl component="fieldset">
-                <FormLabel component="legend">실내/실외</FormLabel>
-                <RadioGroup
-                  row
-                  aria-label="inOutWhether"
-                  name="inout-whether"
-                  value={inOutWhether}
-                  onChange={(e) => setInOutWhether(parseInt(e.target.value))}
-                >
-                  <FormControlLabel value={1} control={<Radio />} label="실내" />
-                  <FormControlLabel value={0} control={<Radio />} label="실외" />
-                </RadioGroup>
-              </FormControl>
-            </Box>
-            <Box mb={2}>
-              <FormControl component="fieldset">
-                <FormLabel component="legend">잔디 종류</FormLabel>
-                <RadioGroup
-                  row
-                  aria-label="grassWhether"
-                  name="grass-whether"
-                  value={grassWhether}
-                  onChange={(e) => setGrassWhether(parseInt(e.target.value))}
-                >
-                  <FormControlLabel value={1} control={<Radio />} label="천연" />
-                  <FormControlLabel value={0} control={<Radio />} label="인조" />
-                </RadioGroup>
-              </FormControl>
-            </Box>
-            <Box mb={2}>
-              <FormControl component="fieldset">
-                <FormLabel component="legend">화장실 여부</FormLabel>
-                <RadioGroup
-                  row
-                  aria-label="toiletStatus"
-                  name="toilet-status"
-                  value={toiletStatus}
-                  onChange={(e) => setToiletStatus(parseInt(e.target.value))}
-                >
-                  <FormControlLabel value={1} control={<Radio />} label="있음" />
-                  <FormControlLabel value={0} control={<Radio />} label="없음" />
-                </RadioGroup>
-              </FormControl>
-            </Box>
-            <Box mb={2}>
-              <FormControl component="fieldset">
-                <FormLabel component="legend">샤워시설 여부</FormLabel>
-                <RadioGroup
-                  row
-                  aria-label="showerStatus"
-                  name="shower-status"
-                  value={showerStatus}
-                  onChange={(e) => setShowerStatus(parseInt(e.target.value))}
-                >
-                  <FormControlLabel value={1} control={<Radio />} label="있음" />
-                  <FormControlLabel value={0} control={<Radio />} label="없음" />
-                </RadioGroup>
-              </FormControl>
-            </Box>
-            <Box mb={2}>
-              <FormControl component="fieldset">
-                <FormLabel component="legend">주차장 여부</FormLabel>
-                <RadioGroup
-                  row
-                  aria-label="parkingStatus"
-                  name="parking-status"
-                  value={parkingStatus}
-                  onChange={(e) => setParkingStatus(parseInt(e.target.value))}
-                >
-                  <FormControlLabel value={1} control={<Radio />} label="있음" />
-                  <FormControlLabel value={0} control={<Radio />} label="없음" />
-                </RadioGroup>
-              </FormControl>
-            </Box>
-            </div>
-            <div className="required-box" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}> 
-              <div className="required-label">이미지 업로드(테스트중)</div>
+        <form onSubmit={handleSubmit}>
+          <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, marginTop: 2, justifyContent: 'center', alignItems: 'center' }}>
+            <Paper sx={{ display :'flex' , flexDirection :'column' , gap :2 , padding :'50px', width: '700px'}} elevation ={3}>
                 <Box mb={2}>
-                   <label htmlFor="fieldImg1">구장 이미지1 </label>
-                    <input
-                      type="file"
-                      onChange={(e) => handleFileChange(e, setFieldImg1, setFieldImg1Preview)}
-                      className="input-field"
-                    />
-                    {fieldImg1 && <img src={fieldImg1Preview} alt="" className="preview-image" />}
+                  <TextField
+                    value={fieldName}
+                    onChange={(e) =>setFieldName(e.target.value)}
+                    label="구장 이름"
+                    variant="outlined"
+                    sx={{width:700}}
+                  />
                 </Box>
                 <Box mb={2}>
-                  <label htmlFor="fieldImg1">구장 이미지2 </label>
-                    <input
-                      type="file"
-                      onChange={(e) => handleFileChange(e, setFieldImg2, setFieldImg2Preview)}
-                      className="input-field"
-                    />
-                      {fieldImg2 && <img src={fieldImg2Preview} alt="" className="preview-image" />}
+                  <TextField
+                    value={region}
+                    onChange={(e) => setRegion(e.target.value)}
+                    label="지역"
+                    variant="outlined"
+                    sx={{width:700}}
+                  />
                 </Box>
                 <Box mb={2}>
-                  <label htmlFor="fieldImg1">구장 이미지3 </label>
-                    <input
-                      type="file"
-                      onChange={(e) => handleFileChange(e, setFieldImg3, setFieldImg3Preview)}
-                      className="input-field"
-                    />
-                      {fieldImg3 && <img src={fieldImg3Preview} alt="" className="preview-image" />}
+                  <TextField
+                    value={fieldSize}
+                    onChange={(e) => setFieldSize(e.target.value)}
+                    label="구장 크기"
+                    variant="outlined"
+                    sx={{width:700}}
+                  />
                 </Box>
-            </div>
-                <div className="form-container">
-                  <button type="submit" className="submit-button">추가</button>
-                </div>
+                <Box mb={2}>
+                  <TextField
+                    value={reservationFee}
+                    onChange={(e) => setReservationFee(e.target.value)}
+                    label="대관비"
+                    variant="outlined"
+                    sx={{width:700}}
+                  />
+                </Box>
+            </Paper>
+            <Paper sx={{ display :'flex' , flexDirection :'column' , gap :2 , padding :'50px', width: '700px'}} elevation ={3}>
+              <Box mb={2}>
+                <FormControl component="fieldset">
+                  <FormLabel component="legend">실내/실외</FormLabel>
+                  <RadioGroup
+                    row
+                    aria-label="inOutWhether"
+                    name="inout-whether"
+                    value={inOutWhether}
+                    onChange={(e) => setInOutWhether(parseInt(e.target.value))}
+                  >
+                    <FormControlLabel value={1} control={<Radio />} label="실내" />
+                    <FormControlLabel value={0} control={<Radio />} label="실외" />
+                  </RadioGroup>
+                </FormControl>
+              </Box>
+              <Box mb={2}>
+                <FormControl component="fieldset">
+                  <FormLabel component="legend">잔디 종류</FormLabel>
+                  <RadioGroup
+                    row
+                    aria-label="grassWhether"
+                    name="grass-whether"
+                    value={grassWhether}
+                    onChange={(e) => setGrassWhether(parseInt(e.target.value))}
+                  >
+                    <FormControlLabel value={1} control={<Radio />} label="천연" />
+                    <FormControlLabel value={0} control={<Radio />} label="인조" />
+                  </RadioGroup>
+                </FormControl>
+              </Box>
+              <Box mb={2}>
+                <FormControl component="fieldset">
+                  <FormLabel component="legend">화장실 유무</FormLabel>
+                  <RadioGroup
+                    row
+                    aria-label="toiletStatus"
+                    name="toilet-status"
+                    value={toiletStatus}
+                    onChange={(e) => setToiletStatus(parseInt(e.target.value))}
+                  >
+                    <FormControlLabel value={1} control={<Radio />} label="있음" />
+                    <FormControlLabel value={0} control={<Radio />} label="없음" />
+                  </RadioGroup>
+                </FormControl>
+              </Box>
+              <Box mb={2}>
+                <FormControl component="fieldset">
+                  <FormLabel component="legend">샤워시설 유무</FormLabel>
+                  <RadioGroup
+                    row
+                    aria-label="showerStatus"
+                    name="shower-status"
+                    value={showerStatus}
+                    onChange={(e) => setShowerStatus(parseInt(e.target.value))}
+                  >
+                    <FormControlLabel value={1} control={<Radio />} label="있음" />
+                    <FormControlLabel value={0} control={<Radio />} label="없음" />
+                  </RadioGroup>
+                </FormControl>
+              </Box>
+              <Box mb={2}>
+                <FormControl component="fieldset">
+                  <FormLabel component="legend">주차장 여부</FormLabel>
+                  <RadioGroup
+                    row
+                    aria-label="parkingStatus"
+                    name="parking-status"
+                    value={parkingStatus}
+                    onChange={(e) => setParkingStatus(parseInt(e.target.value))}
+                  >
+                    <FormControlLabel value={1} control={<Radio />} label="있음" />
+                    <FormControlLabel value={0} control={<Radio />} label="없음" />
+                  </RadioGroup>
+                </FormControl>
+              </Box>
+            </Paper>
+            <Paper sx={{ display :'flex' , flexDirection :'column' , gap :2 , padding :'50px', width: '700px'}} elevation ={3}>
+                    <Box mb={2}>
+                      <label htmlFor="fieldImg1">구장 이미지1 </label>
+                        <input
+                          type="file"
+                          onChange={(e) => handleFileChange(e, setFieldImg1, setFieldImg1Preview)}
+                          className="input-field"
+                        />
+                        {fieldImg1 && <img src={fieldImg1Preview} alt="" className="preview-image" />}
+                    </Box>
+                    <Box mb={2}>
+                      <label htmlFor="fieldImg1">구장 이미지2 </label>
+                        <input
+                          type="file"
+                          onChange={(e) => handleFileChange(e, setFieldImg2, setFieldImg2Preview)}
+                          className="input-field"
+                        />
+                          {fieldImg2 && <img src={fieldImg2Preview} alt="" className="preview-image" />}
+                    </Box>
+                    <Box mb={2}>
+                      <label htmlFor="fieldImg1">구장 이미지3 </label>
+                        <input
+                          type="file"
+                          onChange={(e) => handleFileChange(e, setFieldImg3, setFieldImg3Preview)}
+                          className="input-field"
+                        />
+                          {fieldImg3 && <img src={fieldImg3Preview} alt="" className="preview-image" />}
+                    </Box>
+              </Paper>
+              <Box>
+                <Button variant="contained" color="primary" type="submit" className="submit-button" style={{ color: '#fff' }}>
+                  등록
+                </Button>
+                <Button variant="outlined" color="secondary" onClick={() => navigate('/admin')} style={{ borderColor: 'green', color: 'green', margin: '20px' }}>
+                  취소
+                </Button>   
+              </Box>
+              <Snackbar open={openSnackbar} autoHideDuration={3000} onClose={() => setOpenSnackbar(false)} anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
+                <Alert severity="success">구장이 성공적으로 생성되었습니다.</Alert>
+              </Snackbar>
+              <Snackbar open={openSnackbar} autoHideDuration={3000} onClose={() => setOpenSnackbar(false)} anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
+                <Alert severity="error">{errorMessage}</Alert>
+              </Snackbar>
+            </Box>
           </form>
       </ThemeProvider>
   );  
