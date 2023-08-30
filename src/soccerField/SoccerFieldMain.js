76a11e2f98b30fa0b8932aa84aa168d3c9431534
DARUDARUDA <aidfire@naver.com>
2023-08-31 04:56:35 +0900

    수정 : 레이아웃 정렬, 기본 아이콘 추가

--- src/soccerField/SoccerFieldMain.js
+++ src/soccerField/SoccerFieldMain.js
@@ -3,6 +3,9 @@ import { useParams } from "react-router-dom";
 import Payment from "../payment/Payment";
 import styles from "./SoccerField.module.css";
 import SoccerFieldImageSlide from "./SoccerFieldImageSlide";
+import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
+import ShowerIcon from '@mui/icons-material/Shower';
+import WcIcon from '@mui/icons-material/Wc';
 
 const SoccerFieldMain = () => {
   let { fieldId } = useParams();
@@ -17,22 +20,59 @@ const SoccerFieldMain = () => {
       });
   }, [fieldId]);
 
+  const on = { color : "#444444", marginBottom : "-3px"};
+
+  const off = { color : "#aaaaaa", marginBottom : "-3px"};
+
   return (
     <div className={styles.container}>
       <SoccerFieldImageSlide />
       {fieldInfo && 
           <div className={styles.flexContainer}>
             <div className={styles.infoContainer}>
-              <h2>경기장 정보</h2>
-              <h2>구장명 : {fieldInfo.fieldName}</h2>
-              <h2>지역 : {fieldInfo.region}</h2>
-              <h2>잔디 : {fieldInfo.grassWhether === "1" ? "자연잔디" : "인조잔디"}</h2>
-              <h2>실내여부 : {fieldInfo.inOutWhether === "1" ? "실내" : "실외"}</h2>
-              <h2>주차장 : {fieldInfo.parkingStatus ? "있음" : "없음"}</h2>
-              <h2>샤워실 : {fieldInfo.showerStatus ? "있음" : "없음"}</h2>
-              <h2>화장실 : {fieldInfo.toiletStatus ? "있음" : "없음"}</h2>
+              <div className={styles.info}>
+                <h4>{fieldInfo.region}</h4>
+                <h3>{fieldInfo.fieldName}</h3>
+                <span>{fieldInfo.fieldSize} / </span>                
+                <span>{fieldInfo.grassWhether === "1" ? "천연잔디 / " : "인조잔디 / "}</span>                
+                <span>{fieldInfo.inOutWhether === "1" ? "실내" : "실외"}</span>                                                      
+              </div>
+              <div className={styles.info}>
+                <h3>시설 이용 정보</h3>
+                <div>
+                  {fieldInfo.parkingStatus ?
+                <>
+                <DirectionsCarIcon sx={on}/> <span className={styles.on}>주차장 - 사용가능</span>
+                </> : 
+                <>
+                <DirectionsCarIcon sx={off}/> <span className={styles.off}>주차장</span>
+                </>
+                }
+                </div>
+                <div>
+                  {fieldInfo.showerStatus ?
+                <>
+                <ShowerIcon sx={on}/> <span className={styles.on}>샤워실 - 사용가능</span>
+                </> : 
+                <>
+                <ShowerIcon sx={off}/> <span className={styles.off}>샤워실</span>
+                </>
+                }
+                </div>
+                <div>
+                  {fieldInfo.toiletStatus ?
+                <>
+                <WcIcon sx={on}/> <span className={styles.on}>화장실 - 사용가능</span>
+                </> : 
+                <>
+                <WcIcon sx={off}/> <span className={styles.off}>화장실</span>
+                </>
+                }
+                </div>
+              </div>                            
             </div>
             <div className={styles.reservationContainer}>
+              <h3>예약하기</h3>
               <Payment
                 fieldId={fieldInfo.id}
                 fieldName={fieldInfo.fieldName}
