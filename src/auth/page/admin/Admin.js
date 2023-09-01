238aa8a07e912d9386ab031a7152ab6bf22dccbb
parksh93 <sanghyun8527@gmail.com>
2023-09-01 17:35:49 +0900

    삭제: merge시 불필요 파일 추가되어 삭제

--- src/auth/page/admin/Admin.js
+++ src/auth/page/admin/Admin.js
@@ -1,13 +1,3 @@
-import FieldReservation from "./tableFieldReservation/FieldReservation";
-import TeamMember from "./tableTeam/TeamMember";
-import FriendAccept from "./tableFriend/FriendAccept";
-import FriendAdd from "./tableFriend/FriendAdd";
-import FriendBlock from "./tableFriend/FriendBlock";
-import FriendList from "./tableFriend/FriendList";
-import MatchTeam from "./tableMatch/MatchTeam";
-import MatchIndividual from "./tableMatch/MatchIndividual";
-import TeamApply from "./tableTeam/TeamApply";
-import TeamMatchResult from "./tableTeam/TeamMatchResult";
 import React from 'react'
 import './Admin.css';
 import SideBar from './sideBar/SideBar';
