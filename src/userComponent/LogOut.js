30ebe19adf0e09c70640ce1bceadeaa3e73471e3
parksh93 <sanghyun8527@gmail.com>
2023-09-02 18:42:50 +0900

    수정: 로그아웃 불필요 변수 삭제

--- src/userComponent/LogOut.js
+++ src/userComponent/LogOut.js
@@ -3,14 +3,13 @@ import { useUser } from './userContext/UserContext';
 
 const LogOut = () => {
     const navigate = useNavigate();
-    const {setUserInfo, setValid} = useUser();
+    const {setUserInfo} = useUser();
 
     const logOut = async () => {
         const response = await fetch("/user/logout",{method: 'POST'});
         const data = await response.json();
         if(data === true){
             setUserInfo(null);
-            setValid(false);
             navigate("/")
         }
     }
