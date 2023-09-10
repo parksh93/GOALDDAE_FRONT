5f1d1f60707e0a86f0ea6a3f491ea9d7a64b24e4
SeoPBi <aktms252@naver.com>
2023-09-10 23:20:50 +0900

    수정: 푸터 반응형 수정

--- src/auth/footer/Footer.js
+++ src/auth/footer/Footer.js
@@ -11,16 +11,22 @@ const Footer = () => {
                 marginTop: "20%",
                 display:"flex", 
                 flexDirection: isSmallScreen ? "column" : "row", 
-                justifyContent:"flex-start"
+                justifyContent:"flex-start",
+                width: isSmallScreen ? "100%" : "1400px"
                 }}>
-                <Box sx= {{fontSize : isSmallScreen ? "12px" : "14px", marginLeft : isSmallScreen ? "10%" : "5%",marginTop: "2%"}}>매치
-                <Box sx={{fontSize : isSmallScreen ? "4px" : "13px", marginTop: isSmallScreen? "2%" : "13%", color:"#969696"}}>
-                    <Link to="/"><Box sx={{marginTop : isSmallScreen ? "1%" : "10%"}}>개인 매치</Box></Link>   
-                    <Link to="/"><Box sx={{marginTop : isSmallScreen ? "1%" : "10%"}}>팀 매치</Box></Link> 
-                    <Link to="/"><Box sx={{marginTop : isSmallScreen ? "1%" : "10%"}}>팀 가입하기</Box></Link> 
-                    <Link to="/"><Box sx={{marginTop : isSmallScreen ? "1%" : "10%"}}>자유게시판</Box></Link> 
-                    <Link to="/"><Box sx={{marginTop : isSmallScreen ? "1%" : "10%"}}>핫이슈</Box></Link> 
-                </Box>
+                <Box sx= {{
+                    fontSize : isSmallScreen ? "12px" : "14px",
+                    marginLeft : isSmallScreen ? "10%" : "5%",
+                    marginTop: "2%"
+                    }}>
+                    매치
+                    <Box sx={{fontSize : isSmallScreen ? "4px" : "13px", marginTop: isSmallScreen? "2%" : "13%", color:"#969696"}}>
+                        <Link to="/"><Box sx={{marginTop : isSmallScreen ? "1%" : "10%"}}>개인 매치</Box></Link>   
+                        <Link to="/"><Box sx={{marginTop : isSmallScreen ? "1%" : "10%"}}>팀 매치</Box></Link> 
+                        <Link to="/"><Box sx={{marginTop : isSmallScreen ? "1%" : "10%"}}>팀 가입하기</Box></Link> 
+                        <Link to="/"><Box sx={{marginTop : isSmallScreen ? "1%" : "10%"}}>자유게시판</Box></Link> 
+                        <Link to="/"><Box sx={{marginTop : isSmallScreen ? "1%" : "10%"}}>핫이슈</Box></Link> 
+                    </Box>
                 </Box>
                 <Box sx= {{
                     fontSize : isSmallScreen ? "12px" : "14px", 
