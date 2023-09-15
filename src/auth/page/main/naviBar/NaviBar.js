ee879a1561b91e0c2621c201b3b56721d7a596c9
SeoPBi <aktms252@naver.com>
2023-09-15 22:47:53 +0900

    미비된 사항 작업 중

--- src/auth/page/main/naviBar/NaviBar.js
+++ src/auth/page/main/naviBar/NaviBar.js
@@ -14,81 +14,59 @@ const NaviBar = () => {
 
   return (
     <>
-          <Box sx={{ bgcolor:"#4BAF4B", color:"white", display: "flex",justifyContent:"center" }}>
+        <Box sx={{ bgcolor:"#4BAF4B", color:"white", display: "flex",justifyContent:"center" }}>
           <Button 
             sx={{ 
                 color:"white",
                 fontSize: isSmallScreen ? "12px" : "15px",
-                marginRight: isSmallScreen ? "8px" : "40px",
+                marginRight: isSmallScreen ? "8px" : "1px",
                 paddingX: isSmallScreen ? "10px" : "50px",
                 backgroundColor: location.pathname === '/' ? '#64CD3C' : 'transparent', 
-                '&:hover': {
-                backgroundColor: '#64CD3C'
-                },
-            }}
-            onClick={() => navigate('/')}
-            >
-            개인매치
+                '&:hover': {backgroundColor: '#64CD3C'}
+            }}onClick={() => navigate('/')}>개인매치
             </Button>
+
             <Button 
-            sx={{ 
+                sx={{ 
+                    color:"white",
+                    fontSize: isSmallScreen ? "12px" : "15px",
+                    marginRight: isSmallScreen ? "8px" : "1px",
+                    paddingX: isSmallScreen ? "10px" : "58px",
+                    backgroundColor: location.pathname === '/match/team/list' ? '#64CD3C' : 'transparent', 
+                    '&:hover': {backgroundColor: '#64CD3C'}
+                }} onClick={() => navigate('/match/team/list')}>팀매치
+            </Button>
+
+            <Button 
+                sx={{ 
+                    color:"white",
+                    fontSize: isSmallScreen ? "12px" : "15px",
+                    marginRight: isSmallScreen ? "8px" : "1px",
+                    paddingX: isSmallScreen ? "10px" : "68px",
+                    backgroundColor: location.pathname === '/team/list' ? '#64CD3C' : 'transparent', 
+                    '&:hover': {backgroundColor: '#64CD3C'}
+                }} onClick={() => navigate('/team/list')}>팀
+            </Button>
+
+            <Button 
+                sx={{ 
                 color:"white",
                 fontSize: isSmallScreen ? "12px" : "15px",
-                marginRight: isSmallScreen ? "8px" : "40px",
-                paddingX: isSmallScreen ? "10px" : "50px",
-                backgroundColor: location.pathname === '/match/team/list' ? '#64CD3C' : 'transparent', 
-                '&:hover': {
-                backgroundColor: '#64CD3C'
-                },
-            }}
-            onClick={() => navigate('/match/team/list')}
-            >
-            팀매치
-            </Button>
-            <Button 
-            sx={{ 
-                color:"white",
-                fontSize: isSmallScreen ? "12px" : "15px",
-                marginRight: isSmallScreen ? "8px" : "40px",
-                paddingX: isSmallScreen ? "10px" : "50px",
-                backgroundColor: location.pathname === '/team/list' ? '#64CD3C' : 'transparent', 
-                '&:hover': {
-                backgroundColor: '#64CD3C',
-                },
-            }}
-            onClick={() => navigate('/team/list')}
-            >
-            팀
-            </Button>
-            <Button 
-            sx={{ 
-                color:"white",
-                fontSize: isSmallScreen ? "12px" : "15px",
-                marginRight: isSmallScreen ? "8px" : "40px",
+                marginRight: isSmallScreen ? "8px" : "1px",
                 paddingX: isSmallScreen ? "10px" : "50px",
                 backgroundColor: location.pathname === '/reservation/list' ? '#64CD3C' : 'transparent', 
-                '&:hover': {
-                backgroundColor: '#64CD3C',
-                },
-            }}
-            onClick={() => navigate('/reservation/list')}
-            >
-            구장예약
+                '&:hover': {backgroundColor: '#64CD3C'}
+                }} onClick={() => navigate('/reservation/list')}>구장예약
             </Button>
+
             <Button 
-            sx={{ 
-                color:"white",
-                fontSize: isSmallScreen ? "12px" : "15px",
-                marginRight: isSmallScreen ? "8px" : "40px",
-                paddingX: isSmallScreen ? "10px" : "50px",
-                backgroundColor: location.pathname === '/board' ? '#64CD3C' : 'transparent', 
-                '&:hover': {
-                backgroundColor: '#64CD3C',
-                },
-            }}
-            onClick={() => navigate('/board')}
-            >
-            게시판
+                sx={{ 
+                    color:"white",
+                    fontSize: isSmallScreen ? "12px" : "15px",
+                    paddingX: isSmallScreen ? "10px" : "58px",
+                    backgroundColor: location.pathname === '/board' ? '#64CD3C' : 'transparent', 
+                    '&:hover': {backgroundColor: '#64CD3C'}
+                }} onClick={() => navigate('/board')}>게시판
             </Button>
         </Box>
     </>
--- src/auth/page/reservationList/ReservationList.js
+++ src/auth/page/reservationList/ReservationList.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import { useUser } from '../../../userComponent/userContext/UserContext';
 import NaviBar from '../main/naviBar/NaviBar';
 import { Box, Chip } from '@mui/material';
 import FieldImg1 from './FieldImg1.jpeg';
 import { grey } from '@mui/material/colors';
+import { Link } from 'react-router-dom';
 
 const provinces = [
   "서울", "경기", "인천", "강원", "대전",
@@ -22,6 +23,8 @@ const ReservationList = () => {
   const [fields, setFields] = useState([]);
   const [reservationDate,setReservationDate] = useState(new Date().toISOString().slice(0,10));
   const [reservationPeriod, setReservationPeriod] = useState('');
+  const [page, setPage] = useState(1);
+  const loadingRef = useRef(null);
 
   useEffect(() => {
     if (selectedProvince) {
@@ -47,80 +50,146 @@ const ReservationList = () => {
     setSelectedProvince(preferredCity);
   }, [userInfo]);
 
+
 return (
-    <div> 
-      <NaviBar />
-        <select value={selectedProvince} onChange={e => setSelectedProvince(e.target.value)} style={{marginTop:'2%', marginLeft:'15%'}}> 
-        <option value="">지역 선택</option> {provinces.map(province => <option key={province} value={province}>{province}</option> )} </select>
-
-        {/* 일자 선택 */}
-        <label>
-          <input type="date" value={reservationDate} onChange={(e) => setReservationDate(e.target.value)} style={{marginLeft:'1%'}}/>
-        </label>
-
-        <select value={reservationPeriod} onChange={(e) => setReservationPeriod(e.target.value)} style={{marginLeft:'1%'}}>
-          <option value="">시간대 선택</option>
-          <option value="오전">오전(06:00 ~ 12:00)</option>
-          <option value="오후">오후(12:00 ~ 18:00)</option>
-          <option value="저녁">저녁(18:00 ~ 24:00)</option>
-        </select>
-
-        <select value={inOutWhether} onChange={(e) => setInOutWhether(e.target.value)} style={{marginLeft:'1%'}}>
-          <option value="">실내외 선택</option>
-          <option value="실내">실내</option>
-          <option value="실외">실외</option>
-        </select>
-
-      <select value={grassWhether} onChange={(e) => setGrassWhether(e.target.value)} style={{marginLeft:'1%'}}>
-        <option value="">잔디 유형 선택</option>
-        <option value="천연">천연 잔디</option>
-        <option value="인조">인조 잔디</option>
+  <div> 
+    <NaviBar />
+    <select
+        value={selectedProvince}
+        onChange={e => setSelectedProvince(e.target.value)}
+        style={{
+          marginTop: '2%',
+          marginLeft: '15%',
+          padding: '8px',
+          borderRadius: '4px',
+          borderColor: '#ccc',
+          fontSize: '16px',
+        }}
+      >
+        <option value="">지역 선택</option>
+        {provinces.map(province => (
+          <option key={province} value={province}>
+            {province}
+          </option>
+        ))}
       </select>
+
+      <label>
+        <input
+          type="date"
+          value={reservationDate}
+          onChange={(e) => setReservationDate(e.target.value)}
+          style={{
+            marginLeft: '1%',
+            padding: '8px',
+            borderRadius: '4px',
+            border: '1px solid #ccc', 
+            fontSize: '16px',
+            outline: 'none'
+          }}
+        />
+      </label>
+
+      <select
+        value={reservationPeriod}
+        onChange={(e) => setReservationPeriod(e.target.value)}
+        style={{
+          marginLeft: '1%',
+          padding: '8px',
+          borderRadius: '4px',
+          borderColor: '#ccc',
+          fontSize: '16px',
+        }}
+      >
+        <option value="">시간대 선택</option>
+        <option value="오전">오전(06:00 ~ 12:00)</option>
+        <option value="오후">오후(12:00 ~ 18:00)</option>
+        <option value="저녁">저녁(18:00 ~ 24:00)</option>
+      </select>
+
+      <select
+        value={inOutWhether}
+        onChange={(e) => setInOutWhether(e.target.value)}
+        style={{
+          marginLeft: '1%',
+          padding: '8px',
+          borderRadius: '4px',
+          borderColor: '#ccc',
+          fontSize: '16px',
+        }}
+      >
+        <option value="">실내외 선택</option>
+        <option value="실내">실내</option>
+        <option value="실외">실외</option>
+      </select>
+
+    <select
+      value={grassWhether}
+      onChange={(e) => setGrassWhether(e.target.value)}
+      style={{
+        marginLeft: '1%',
+        padding: '8px',
+        borderRadius: '4px',
+        borderColor: '#ccc',
+        fontSize: '16px',
+      }}
+    >
+      <option value="">잔디 유형 선택</option>
+      <option value="천연">천연 잔디</option>
+      <option value="인조">인조 잔디</option>
+    </select>
     
-      {fields.map(field =>
-      <Box key={field.id} sx={{marginLeft:'15%', borderBottom: `1px solid ${grey[500]}`}}>
-        <Box sx={{marginTop:'2%', fontSize:'20px', fontWeight:'bold'}}>{field.fieldName}</Box>
-        <Box sx={{fontSize:'13px', color: grey[600]}}>
-          운영 시간: {field.operatingHours.split(':').slice(0, 2).join(':')} ~ {field.closingTime.split(':').slice(0, 2).join(':')}</Box>
-        <Box sx={{fontSize:'13px', display: 'flex', justifyContent:'flex-start', color: grey[600]}}>
-          {field.toiletStatus && <Box>화장실&middot;</Box>}
-          {field.showerStatus && <Box>샤워실&middot;</Box>}
-          {field.parkingStatus && <Box>주차장</Box>}
-        </Box>
-        <Box sx={{ display: 'flex', alignItems: 'start' }}>
-          <Box sx={{fontSize:'13px', color: grey[600]}}>
-            <Box>장소: {field.inOutWhether}</Box>
-            <Box>잔디 종류: {field.grassWhether}</Box>
-            <Box>구장 크기: {field.fieldSize}</Box>
-            <Box>대관비: {field.reservationFee}</Box>
-          </Box>
-          <img src={FieldImg1} alt="구장 이미지" style={{width:'10%', marginLeft:'57%'}}/>
-        </Box>  
-
-        {field.reservationInfo && (
-          <>
-            <Box sx={{marginTop:'2%', fontSize:'14px', fontWeight:'bold'}}>예약 가능</Box>
-            <Box sx={{display: 'flex', flexWrap: 'wrap'}}>
-              {field.reservationInfo.availableTimes.map(time => 
-                <Box key={time} sx={{margin: '5px'}}>
-                  <Chip label={time.split(':').slice(0, 2).join(':')} sx={{backgroundColor: '#2196f3', color: '#fff'}}/>
-                </Box>)}
+    {fields.map((field, index) =>
+      <div key={field.id} ref={index === fields.length - 1 ? loadingRef : null}>     
+        <Link to={`/soccer_field/${field.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+          <Box key={field.id} sx={{marginLeft:'15%', borderBottom: `1px solid ${grey[500]}`, width: '50%'}}>
+            <Box sx={{marginTop:'2%', fontSize:'20px', fontWeight:'bold'}}>
+              {field.fieldName}
             </Box>
-
-            {field.reservationInfo.reservedTimes.length > 0 && (
-              <>
-                <Box sx={{marginTop:'1%', fontSize:'14px', fontWeight:'bold'}}>예약 현황</Box>
-                <Box sx={{display: 'flex', flexWrap: 'wrap', marginBottom: '20px'}}>
-                  {field.reservationInfo.reservedTimes.map(time => 
-                    <Box key={time} sx={{margin: '5px'}}>
-                      <Chip label={time.split(':').slice(0, 2).join(':')} sx={{backgroundColor: '#f44336', color:'#fff'}} />
-                    </Box>)}
+              <Box sx={{fontSize:'13px', color: grey[600]}}>
+                운영 시간: {field.operatingHours.split(':').slice(0, 2).join(':')} ~ {field.closingTime.split(':').slice(0, 2).join(':')}</Box>
+              <Box sx={{fontSize:'13px', display: 'flex', justifyContent:'flex-start', color: grey[600]}}>
+                {field.toiletStatus && <Box>화장실&middot;</Box>}
+                {field.showerStatus && <Box>샤워실&middot;</Box>}
+                {field.parkingStatus && <Box>주차장</Box>}
+              </Box>
+              <Box sx={{ display: 'flex', alignItems: 'start' }}>
+                <Box sx={{fontSize:'13px', color: grey[600]}}>
+                  <Box>장소: {field.inOutWhether}</Box>
+                  <Box>잔디 종류: {field.grassWhether}</Box>
+                  <Box>구장 크기: {field.fieldSize}</Box>
+                  <Box>대관비: {field.reservationFee}</Box>
                 </Box>
-              </>
-            )}
-          </>
-        )}
-      </Box>)}
+                <img src={FieldImg1} alt="구장 이미지" style={{width:'10%', marginLeft:'57%'}}/>
+              </Box>  
+
+              {field.reservationInfo && (
+                <>
+                  <Box sx={{marginTop:'0%', fontSize:'14px', fontWeight:'bold'}}>예약 가능</Box>
+                  <Box sx={{display: 'flex', flexWrap: 'wrap'}}>
+                    {field.reservationInfo.availableTimes.map(time => 
+                      <Box key={time} sx={{margin: '5px'}}>
+                        <Chip label={time.split(':').slice(0, 2).join(':')} sx={{backgroundColor: '#2196f3', color: '#fff'}}/>
+                      </Box>)}
+                  </Box>
+
+                  {field.reservationInfo.reservedTimes.length > 0 && (
+                    <>
+                      <Box sx={{marginTop:'1%', fontSize:'14px', fontWeight:'bold'}}>예약 현황</Box>
+                      <Box sx={{display: 'flex', flexWrap: 'wrap', marginBottom: '20px'}}>
+                        {field.reservationInfo.reservedTimes.map(time => 
+                          <Box key={time} sx={{margin: '5px'}}>
+                            <Chip label={time.split(':').slice(0, 2).join(':')} sx={{backgroundColor: '#f44336', color:'#fff'}} />
+                          </Box>)}
+                      </Box>
+                    </>
+                  )}
+                </>
+              )}
+          </Box>
+        </Link>
+      </div>)}
     </div> 
   )}
+
 export default ReservationList;
