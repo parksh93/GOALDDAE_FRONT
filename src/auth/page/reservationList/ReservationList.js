14358c1bbd5bd56c088079329b4ddaf75b5b1ae2
SeoPBi <aktms252@naver.com>
2023-09-16 17:46:21 +0900

    수정: 무한스크롤 구현 완료

--- src/auth/page/reservationList/ReservationList.js
+++ src/auth/page/reservationList/ReservationList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import axios from 'axios';
 import { useUser } from '../../../userComponent/userContext/UserContext';
 import NaviBar from '../main/naviBar/NaviBar';
@@ -23,7 +23,8 @@ const ReservationList = () => {
   const [fields, setFields] = useState([]);
   const [reservationDate,setReservationDate] = useState(new Date().toISOString().slice(0,10));
   const [reservationPeriod, setReservationPeriod] = useState('');
-  const loadingRef = useRef(null);
+  const [pageNumber, setPageNumber] = useState(1);
+  const pageSize = 10; 
 
   useEffect(() => {
     if (selectedProvince) {
@@ -34,14 +35,42 @@ const ReservationList = () => {
           grassWhether,
           province : selectedProvince,
           reservationDate,
-          reservationPeriod
+          reservationPeriod,
+          pageNumber, 
+          pageSize   
         }
       })
-        .then(response => setFields(response.data))
-        .catch(error => console.error(`Error: ${error}`));
+      .then(response => {
+        if (pageNumber === 1) {
+            setFields(response.data.content); // 첫 페이지일 경우에는 새로운 데이터만 설정
+        } else {
+            setFields(prevFields => [...prevFields, ...response.data.content]); // 그 외의 경우에는 이전 데이터와 합침
+        }
+      })
+      .catch(error => console.error(`Error: ${error}`));
     }
-    
-  }, [selectedProvince, inOutWhether, grassWhether, reservationDate, reservationPeriod]);
+  }, [selectedProvince, inOutWhether, grassWhether, reservationDate, reservationPeriod, pageNumber]);
+  
+  useEffect(() => {
+    if (selectedProvince !== defaultProvince) { // defaultProvince와 다르면 fields와 pageNumber 초기화
+      setFields([]);
+      setPageNumber(1);
+    }
+  }, [selectedProvince]);
+  
+  const observer = useRef();
+  const lastFieldElementRef = useCallback(
+    (node) => {
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver(entries => {
+        if (entries[0].isIntersecting) {
+          setPageNumber(prevPageNumber => prevPageNumber + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    []
+  );
 
   // 페이지 새로고침해도 지역 유지  
   useEffect(() => {
@@ -50,102 +79,104 @@ const ReservationList = () => {
   }, [userInfo]);
 
 return (
-  <div> 
-    <NaviBar />
-    <select
-        value={selectedProvince}
-        onChange={e => setSelectedProvince(e.target.value)}
-        style={{
-          marginTop: '2%',
-          marginLeft: '15%',
-          padding: '8px',
-          borderRadius: '4px',
-          borderColor: '#ccc',
-          fontSize: '14px',
-        }}
-      >
-        <option value="">지역 선택</option>
-        {provinces.map(province => (
-          <option key={province} value={province}>
-            {province}
-          </option>
-        ))}
-      </select>
+    <div> 
+      <NaviBar />
+      <select
+          value={selectedProvince}
+          onChange={e => setSelectedProvince(e.target.value)}
+          style={{
+            marginTop: '2%',
+            marginLeft: '15%',
+            padding: '8px',
+            borderRadius: '4px',
+            borderColor: '#ccc',
+            fontSize: '14px',
+          }}
+        >
+          <option value="">지역 선택</option>
+          {provinces.map(province => (
+            <option key={province} value={province}>
+              {province}
+            </option>
+          ))}
+        </select>
 
-      <label>
-        <input
-          type="date"
-          value={reservationDate}
-          onChange={(e) => setReservationDate(e.target.value)}
+        <label>
+          <input
+            type="date"
+            value={reservationDate}
+            onChange={(e) => setReservationDate(e.target.value)}
+            style={{
+              marginLeft: '1%',
+              padding: '8px',
+              borderRadius: '4px',
+              border: '1px solid #ccc', 
+              fontSize: '14px',
+              outline: 'none'
+            }}
+          />
+        </label>
+
+        <select
+          value={reservationPeriod}
+          onChange={(e) => setReservationPeriod(e.target.value)}
           style={{
             marginLeft: '1%',
             padding: '8px',
             borderRadius: '4px',
-            border: '1px solid #ccc', 
+            borderColor: '#ccc',
             fontSize: '14px',
-            outline: 'none'
           }}
-        />
-      </label>
+        >
+          <option value="">시간대 선택</option>
+          <option value="오전">오전(06:00 ~ 12:00)</option>
+          <option value="오후">오후(12:00 ~ 18:00)</option>
+          <option value="저녁">저녁(18:00 ~ 24:00)</option>
+        </select>
 
-      <select
-        value={reservationPeriod}
-        onChange={(e) => setReservationPeriod(e.target.value)}
-        style={{
-          marginLeft: '1%',
-          padding: '8px',
-          borderRadius: '4px',
-          borderColor: '#ccc',
-          fontSize: '14px',
-        }}
-      >
-        <option value="">시간대 선택</option>
-        <option value="오전">오전(06:00 ~ 12:00)</option>
-        <option value="오후">오후(12:00 ~ 18:00)</option>
-        <option value="저녁">저녁(18:00 ~ 24:00)</option>
-      </select>
+        <select
+          value={inOutWhether}
+          onChange={(e) => setInOutWhether(e.target.value)}
+          style={{
+            marginLeft: '1%',
+            padding: '8px',
+            borderRadius: '4px',
+            borderColor: '#ccc',
+            fontSize: '14px',
+          }}
+        >
+          <option value="">실내외 선택</option>
+          <option value="실내">실내</option>
+          <option value="실외">실외</option>
+        </select>
 
-      <select
-        value={inOutWhether}
-        onChange={(e) => setInOutWhether(e.target.value)}
-        style={{
-          marginLeft: '1%',
-          padding: '8px',
-          borderRadius: '4px',
-          borderColor: '#ccc',
-          fontSize: '14px',
-        }}
-      >
-        <option value="">실내외 선택</option>
-        <option value="실내">실내</option>
-        <option value="실외">실외</option>
-      </select>
-
-    <select
-      value={grassWhether}
-      onChange={(e) => setGrassWhether(e.target.value)}
-      style={{
-        marginLeft: '1%',
-        padding: '8px',
-        borderRadius: '4px',
-        borderColor: '#ccc',
-        fontSize: '14px',
-      }}
-    >
-      <option value="">잔디 유형 선택</option>
-      <option value="천연">천연 잔디</option>
-      <option value="인조">인조 잔디</option>
-    </select>
+        <select
+          value={grassWhether}
+          onChange={(e) => setGrassWhether(e.target.value)}
+          style={{
+            marginLeft: '1%',
+            padding: '8px',
+            borderRadius: '4px',
+            borderColor: '#ccc',
+            fontSize: '14px',
+          }}
+        >
+          <option value="">잔디 유형 선택</option>
+          <option value="천연">천연 잔디</option>
+          <option value="인조">인조 잔디</option>
+        </select>
     
-    {fields.map((field, index) =>
-      <div key={field.id} ref={index === fields.length - 1 ? loadingRef : null}>     
-        <Link to={`/soccer_field/${field.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-          <Box key={field.id} sx={{marginLeft:'15%', borderBottom: `1px solid ${grey[500]}`, width: '1100px'}}>
+      {fields.map((field, index) =>
+        <div key={field.id} ref={index === fields.length - 1 ? lastFieldElementRef : null}>
+            <Box key={field.id} sx={{marginLeft:'15%', borderBottom: `1px solid ${grey[500]}`, width: '1100px', marginTop:'40px'}}>
             <Box sx={{marginTop:'2%', fontSize:'20px', fontWeight:'bold'}}>
-              {field.fieldName}
+              <Link to={`/soccer_field/${field.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                {field.fieldName}
+              </Link>
             </Box>
               <Box sx={{fontSize:'13px', color: grey[600]}}>
-                운영 시간: {field.operatingHours.split(':').slice(0, 2).join(':')} ~ {field.closingTime.split(':').slice(0, 2).join(':')}</Box>
+                운영 시간: {field.operatingHours.split(':').slice(0, 2).join(':')} ~ {field.closingTime.split(':').slice(0, 2).join(':')}
+              </Box>
               <Box sx={{fontSize:'13px', display: 'flex', justifyContent:'flex-start', color: grey[600]}}>
                 {field.toiletStatus && <Box>화장실&middot;</Box>}
                 {field.showerStatus && <Box>샤워실&middot;</Box>}
@@ -165,16 +196,18 @@ return (
                 <>
                   <Box sx={{marginTop:'0%', fontSize:'14px', fontWeight:'bold'}}>예약 가능</Box>
                   <Box sx={{display: 'flex', flexWrap: 'wrap'}}>
-                    {field.reservationInfo.availableTimes.map(time => 
+                    <Link to={`/soccer_field/${field.id}`} style={{ textDecoration: 'none', color: 'inherit',display: 'flex', alignItems: 'start' }}>
+                      {field.reservationInfo.availableTimes.map(time => 
                       <Box key={time} sx={{margin: '5px'}}>
                         <Chip label={time.split(':').slice(0, 2).join(':')} sx={{backgroundColor: '#2196f3', color: '#fff'}}/>
                       </Box>)}
+                    </Link>
                   </Box>
 
                   {field.reservationInfo.reservedTimes.length > 0 && (
                     <>
                       <Box sx={{marginTop:'1%', fontSize:'14px', fontWeight:'bold'}}>예약 현황</Box>
-                      <Box sx={{display: 'flex', flexWrap: 'wrap', marginBottom: '20px'}}>
+                      <Box sx={{display: 'flex', flexWrap: 'wrap', marginBottom: '30px'}}>
                         {field.reservationInfo.reservedTimes.map(time => 
                           <Box key={time} sx={{margin: '5px'}}>
                             <Chip label={time.split(':').slice(0, 2).join(':')} sx={{backgroundColor: '#f44336', color:'#fff'}} />
@@ -184,9 +217,8 @@ return (
                   )}
                 </>
               )}
-          </Box>
-        </Link>
-      </div>)}
+            </Box>
+       </div>)}
     </div> 
   )}
 
