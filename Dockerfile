9d1c84799de8e78f6d8cd8af04a84bd2822bb5f6
parksh93 <sanghyun8527@gmail.com>
2023-09-25 18:39:45 +0900

    수정: 빌드 안되는 문제 해결

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,14 @@
-FROM node:18
+FROM node
+
 WORKDIR /app
-COPY package.json package-lock.json ./
-RUN npm install -g
-COPY . .
+
+COPY package.json ./
+
+RUN npm install --force
+# RUN npm install react@latest react-dom@latest
+
+COPY . ./
+
 EXPOSE 3000
-CMD ["npm", "start"]
+
+CMD [ "npm", "start" ]
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom'; 
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,10 +8,12 @@ import { UserProvider } from './userComponent/userContext/UserContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <UserProvider>
-      <App />
-    </UserProvider>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
 reportWebVitals();
